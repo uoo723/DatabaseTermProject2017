caad7308fe582a68d1610a290c911c6faeb6396d
Han Sangwoo <goldhightree@gmail.com>
2017-06-12 08:40:47 +0900

    Add sql script

---
+++ sql/drop.sql
@@ -0,0 +1,7 @@
+drop sequence id_seq;
+drop table payment cascade constraints;
+drop table customer cascade constraints;
+drop table employee cascade constraints;
+drop table "TABLE" cascade constraints;
+drop table menu cascade constraints;
+drop table "ORDER" cascade constraints;
