e152bcdfdafc32b064fd8757c68bb2f6a3c1e1b9
Han Sangwoo <goldhightree@gmail.com>
2017-06-12 00:15:41 +0900

    Resolve Toast NullPointerException

--- app/src/main/java/com/sangwoo/possystem/ui/main/admin/AdminView.java
+++ app/src/main/java/com/sangwoo/possystem/ui/main/admin/AdminView.java
@@ -5,6 +5,8 @@ import com.sangwoo.possystem.ui.main.admin.customer.CustomerTab;
 import com.sangwoo.possystem.ui.main.admin.employee.EmployeeTab;
 import com.sangwoo.possystem.ui.main.admin.menu.MenuTab;
 import com.sangwoo.possystem.ui.main.admin.sales.SalesTab;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -12,10 +14,17 @@ import java.awt.*;
 
 public class AdminView extends BasePanel {
 
+    private static final Logger logger = LogManager.getLogger();
+
     private JLabel titleLabel;
     private JPanel contentPanel;
 
     private JTabbedPane tabbedPane;
+
+    private CustomerTab customerTab;
+    private SalesTab salesTab;
+    private EmployeeTab employeeTab;
+    private MenuTab menuTab;
 
     @Override
     public void initView() {
@@ -23,6 +32,11 @@ public class AdminView extends BasePanel {
 
         titleLabel = new JLabel("등록/조회");
         contentPanel = new JPanel();
+
+        customerTab = new CustomerTab();
+        salesTab = new SalesTab();
+        employeeTab = new EmployeeTab();
+        menuTab = new MenuTab();
 
         Border padding = BorderFactory.createEmptyBorder(1, 1, 1, 1);
         Border lineBorder = BorderFactory.createLineBorder(Color.black);
@@ -40,18 +54,18 @@ public class AdminView extends BasePanel {
     @Override
     public void setTitle(String title) { /* Do nothing */ }
 
+    @Override
+    public void setParentJFrame(JFrame parent) {
+        super.setParentJFrame(parent);
+
+        customerTab.setParentJFrame(parent);
+        salesTab.setParentJFrame(parent);
+        employeeTab.setParentJFrame(parent);
+        menuTab.setParentJFrame(parent);
+    }
+
     private void initContent() {
         tabbedPane = new JTabbedPane();
-
-        CustomerTab customerTab = new CustomerTab();
-        SalesTab salesTab = new SalesTab();
-        EmployeeTab employeeTab = new EmployeeTab();
-        MenuTab menuTab = new MenuTab();
-
-        customerTab.setParentJFrame(getParentJFrame());
-        salesTab.setParentJFrame(getParentJFrame());
-        employeeTab.setParentJFrame(getParentJFrame());
-        menuTab.setParentJFrame(getParentJFrame());
 
         tabbedPane.addTab("고객", customerTab);
         tabbedPane.addTab("매출", salesTab);
